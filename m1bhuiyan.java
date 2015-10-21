2153bb67f4681ad598ced9697624d88c507ee910
Nayeem <exven19@gmail.com>
2015-10-21 14:23:23 -0400

    Update m1bhuiyan.java

--- m1bhuiyan.java
+++ m1bhuiyan.java
@@ -48,7 +48,7 @@ void setup(){
 
 void draw(){
   background( 200, 200, 275);
-  rectmode( CORNERS );
+  rectMode( CORNERS );
   table( left, top, right, bottom );
   buttons();
   bounce();
@@ -73,7 +73,7 @@ void table( float north, float west, float east, float south ){
   rect( east-40, north-40, west+40, south+40 );
   
   //if wall hits
-  float tmp,north,south,namX=0;
+  float tmp,namX=0;
   
   
   //start with a Wall down the middle of the table!
@@ -88,15 +88,17 @@ void table( float north, float west, float east, float south ){
 
 //Action: bounce off walls, collisions
 void bounce(){
+/*
   namX += namDX; if ( namX<west || namX>east ) namDX *= -2;
   namY += namDY; if ( namyY<west || namY>east ) namDY *= -2;
   dinX += dinDX; if ( dinX<west || dinX>east ) dinDX *= -2;
   dinY += dinDY; if ( dinY<west || dinY>east ) dinDY *= -2;
   yanX += yanDX; if ( yanX<west || yanX>east ) yanDX *= -2;
   yanY += yanDY; if ( yanY<west || yanY>east ) yanDY *= -2;
+*/
 }
 
-void collision(){
+void collisions(){
   float east,west;
   //Swap velocities!
   if ( dist( namX, namY, dinX, dinY ) <40 ){
@@ -108,13 +110,13 @@ void collision(){
     float yanY = yanDX; yanDX = namDX; namDX = yanY;
     float namY = namDY; namDY = yanDY; yanDY = namY;
   }
-
+}
 // show: balls, messages, etc.
 void show() { //can't find the error??
   fill( 200, 0, 0); ellipse( namX, namY, 40, 39.99 );
   fill( 0, 200, 0); ellipse( dinX, dinY, 40, 40 );
-  fill( 250, 0 250); ellipse( yanX, yanY, 40, 40 );
-
+  fill( 250, 0, 250); ellipse( yanX, yanY, 40, 40 );
+}
 void messages(){
   fill(1);
   text( title, width/3, 30);
@@ -122,6 +124,8 @@ void messages(){
   text( author, 20, height-10 );
 }
 
+void buttons(){
+}
 
 
 
